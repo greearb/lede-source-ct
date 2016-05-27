c02e49184f2cc6284440d7f27ad5229aa807959d
Ben Greear <greearb@candelatech.com>
2016-05-27 11:09:54 -0700

    Add script to build common platforms.
    The idea is to be able to allow newbies to easily build images
    for common hardware.  These images should be user-friendly, including
    luci and other tools that may aid debugging and use of the platform.
    
    Includes example that builds x86-64 with additional drivers and luci
    so that it works nicely on apu2 and other generic hardware.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

---
+++ buildme.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+USAGE=$( cat <<EOF
+Usage:\n
+# -t:  Target\n
+# -l:  List available targets.\n
+# -f:  Skip feeds (useful on rebuild)\n
+# -j:  Compile jobs (default is 8)\n
+# -h:  Show help and exit\n
+#\n
+#\n
+# Example:  Build an image for x86_64
+#  ./buildme.sh -t x86_64\n
+EOF
+)
+
+BMT=buildme_targets
+TARGET=UNKNOWN
+LIST_TARGETS=0
+SKIP_FEEDS=0
+JOBS=8
+
+while getopts "t:j:lfh" flag
+  do
+  case $flag in
+      t) TARGET=$OPTARG;;
+      l) LIST_TARGETS=1;;
+      f) SKIP_FEEDS=1;;
+      j) JOBS=$OPTARG;;
+      h) echo -e USAGE && exit 0;;
+
+      *) echo "Un-known option: $flag"; echo -e $USAGE;exit 1;;
+  esac
+done
+
+if [ $LIST_TARGETS == 1 ]
+then
+    ls $BMT
+    exit 0
+fi
+
+echo "Target dir: $BMT/$TARGET"
+
+if [ ! -d $BMT/$TARGET ]
+then
+    echo "ERROR:  Unknown target, try -l option?"
+    exit 1;
+fi
+
+set -x
+
+if [ $SKIP_FEEDS != 1 ]
+then
+    # Update feeds
+    ./scripts/feeds update -a || exit 1
+
+    # Enable luci web interface
+    ./scripts/feeds install -d y luci || exit 1
+
+    # Enable ethtool for driver info and stats and such
+    ./scripts/feeds install -d y ethtool || exit 1
+fi
+
+# Copy default config into place
+cp $BMT/$TARGET/diffconfig.txt .config || exit 1
+
+# Build complate .config file based on whatever is latest, etc.
+make defconfig || exit 1
+
+# Build
+make v=S -j $JOBS || exit 1
+
+# Try to figure out where the images are at.
+set +x
+. .config > /dev/null 2>&1
+echo "Images may be found at: bin/targets/$CONFIG_TARGET_BOARD/$CONFIG_TARGET_SUBTARGET/"
