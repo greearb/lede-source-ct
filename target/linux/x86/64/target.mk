2a18a4d8af57f9a7404fedb076337ea4a4aa2fea
Ben Greear <greearb@candelatech.com>
2016-11-16 11:39:49 -0800

    buildme:  Fix apu2 target.
    Seems we don't need to select wpa_supplicant that way anymore.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- target/linux/x86/64/target.mk
+++ target/linux/x86/64/target.mk
@@ -9,7 +9,7 @@ DEFAULT_PACKAGES += kmod-ath9k kmod-ath10k kmod-ath9k-htc kmod-ath5k
 DEFAULT_PACKAGES += ath10k-firmware-qca988x ath10k-firmware-qca99x0 ath9k-htc-firmware ath10k-firmware-qca6174
 # Useful general OS packages
 DEFAULT_PACKAGES += bzip2 curl dmesg ethtool getopt hostapd ip-full iperf3 iputils-ping iputils-tracepath iw
-DEFAULT_PACKAGES += nstat relayd tcpdump wpa-cli wpa-supplicant wpa-supplicant-mesh
+DEFAULT_PACKAGES += nstat relayd tcpdump
 
 define Target/Description
         Build images for 64 bit systems including virtualized guests.
